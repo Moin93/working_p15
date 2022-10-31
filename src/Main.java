fa1b76a21329252ee2e13ab3155c8c45219badca
Moin93 <Moinmirmasomi@outlook.com>
2022-10-31 09:18:11 +0100

    putting actionlistener to solve puzzel

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,8 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         tile15 tile15=new tile15();
--- src/tile15.java
+++ src/tile15.java
@@ -8,12 +8,12 @@ import java.util.Collections;
 public class tile15 extends JFrame implements ActionListener {
     JPanel gamePanel = new JPanel();
     JPanel resetPanel = new JPanel();
-    JButton nyttspel = new JButton("Start New Game");
+    JButton new_game_b = new JButton("Start New Game");
     //JButton emptybutton=new JButton("");
     ArrayList<JButton> buttons_list = new ArrayList<>();
-    JButton[] buttons = new JButton[16];
+    //JButton[] buttons = new JButton[16];
 
-    public static JButton placeholder(JButton emptybutton) {
+    public JButton placeholder(JButton emptybutton) {
         return emptybutton;
     }
 
@@ -21,29 +21,29 @@ public class tile15 extends JFrame implements ActionListener {
         setLayout(new BorderLayout());
         add(gamePanel, "Center");
         add(resetPanel, "North");
-        resetPanel.add(nyttspel);
+        resetPanel.add(new_game_b);
         //gamePanel.add(emptybutton);
         gamePanel.setLayout(new GridLayout(4, 4));
-        String nr = "";
-        for (int i = 0; i < buttons.length; i++) {
-            buttons[i] = new JButton();
-            if (i != buttons.length - 1) {
-                buttons[i].setText(Integer.toString(i + 1));
+        //String nr = "";
+        for (int i = 0; i < 16; i++) {
+            buttons_list.add(new JButton());
+            if (i != 15 /*buttons.length - 1*/) {
+                buttons_list.get(i).setText(Integer.toString(i + 1));
             }
             //buttons[i].addActionListener(this);
 
-            gamePanel.add(buttons[i]);
+            gamePanel.add(buttons_list.get(i));
         }
 
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setVisible(true);
         setSize(500, 500);
-        nyttspel.addActionListener(this);
-        buttons[15].setText("");
-        for (JButton buttons : buttons) {
+        new_game_b.addActionListener(this);
+        buttons_list.get(15).setText("");
+        for (JButton buttons : buttons_list) {
             buttons.addActionListener(this);
         }
-        Collections.addAll(buttons_list, buttons);
+        //Collections.addAll(buttons_list, buttons);
 
 
     }
@@ -62,22 +62,41 @@ public class tile15 extends JFrame implements ActionListener {
     public void actionPerformed(ActionEvent e) {
         //int indxnr=0;
 
-        if (e.getSource() == nyttspel) {
+        if (e.getSource() == new_game_b) {
             Collections.shuffle(/*Arrays.asList(buttons)*/buttons_list);
             for (JButton button : buttons_list/*buttons*/) {
                 gamePanel.add(button);
             }
             gamePanel.revalidate();
-        } /*else if (e.getSource() != nyttspel ||
-                e.getSource() != buttons_list.get(emptytile_index())) {
-            for (JButton b : buttons_list
-            ) {
-                if (){
-
-                }
-            }
         }
-*/
+
+        if (e.getSource() != new_game_b ||
+                e.getSource() != buttons_list.get(emptytile_index())) {
+            int choiceindex=0;
+
+                if (e.getSource()==buttons_list.get(emptytile_index()-1)
+                        ||e.getSource()==buttons_list.get(emptytile_index()+1)
+                        ||e.getSource()==buttons_list.get(emptytile_index()-4)
+                        ||e.getSource()==buttons_list.get(emptytile_index()+4)){
+                    String tilenr_str=placeholder((JButton) e.getSource()).getText();
+                    for (JButton b : buttons_list) {
+
+                        if (b.getText().trim().equalsIgnoreCase(tilenr_str)){
+                            break;
+                        }
+                        choiceindex++;
+                    }
+
+                    Collections.swap(buttons_list,choiceindex,emptytile_index());
+                    //choiceindex++;
+                    for (JButton button : buttons_list/*buttons*/) {
+                        gamePanel.add(button);
+                    }
+                    gamePanel.revalidate();
+                }
+
+        }
+
     }
     /*else if (e.getSource()!=buttons[16]){
         Collections.swap(Arrays.asList(buttons),e.hashCode(),15);
@@ -90,7 +109,7 @@ public class tile15 extends JFrame implements ActionListener {
     public int emptytile_index () {
         int indxnr = 0;
         for (int i = 0; i < buttons_list.size(); i++) {
-            buttons_list.get(i).getText();
+            //buttons_list.get(i).getText();
             if (buttons_list.get(i).getText().trim().equalsIgnoreCase("")) {
                 indxnr = i;
             }
